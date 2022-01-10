764bc479b17616fa958bc84d8b75fc807d612444
sunilpanday <sunilpanday.it@gmail.com>
2022-01-10 16:44:20 +0530

    second commit

--- DemoRestApi/src/main/java/com/rest/api/DemoRestApi/repository/EmpRepo.java
+++ DemoRestApi/src/main/java/com/rest/api/DemoRestApi/repository/EmpRepo.java
@@ -1,5 +1,9 @@
 package com.rest.api.DemoRestApi.repository;
 
-public interface EmpRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rest.api.DemoRestApi.entity.Employee;
+
+public interface EmpRepo extends JpaRepository<Employee, Integer>{
 
 }
